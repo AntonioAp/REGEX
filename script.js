172692b699bf462f71a9458171a35395f7d706cc
Juampi <juanpablo.cantuna@gmail.com>
2021-05-19 16:28:39 +0200

    se arregla la funcion checkRace

--- script.js
+++ script.js
@@ -6,17 +6,10 @@ document.getElementById("formulario").addEventListener('submit',function(event){
     let mail = document.getElementById('mail').value;
     let password = document.getElementById('password').value;
     let raza = document.getElementById('raza').value;
-    console.log(mail)
-    console.log(raza)
-    console.log(password)
     console.log(checkRace(raza));
-
 })
 
 function checkRace(raceOrNot){
-    /* let regex_1 = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]{1, }+\.[A-Za-z]{2,4}$/; */
-    let regex_1 = /[^A-Za-z0-9]/;
-    let regex_2 = /elfo|humano|hobbit|enano/;
-    console.log(regex_1.test(raceOrNot), regex_2.test(raceOrNot))
-    return regex_1.test(raceOrNot) &&  regex_2.test(raceOrNot);
+    let regex = /^elfo$|^humano$|^hobbit$|^enano$/;
+    return regex.test(raceOrNot);
 }
